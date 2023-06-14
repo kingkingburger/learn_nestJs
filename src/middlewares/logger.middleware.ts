de56a668cb72522c639e60217142d94b753703aa
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-06-14 20:29:22 +0900

    ADD(⚡): Using loggerMiddleware

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || ''; // 헤더에서 user-agent 하는 속성 가져오는거임
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
+    next();
+  }
+}
