3a1e97655d8bf4da66c629dbad2323f620854862
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-06-17 06:04:04 +0900

    ADD(⚡): Create Module, Controller, Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
 
 const getEnv = async () => {
   // const respose = await axios.get
@@ -13,7 +16,7 @@ const getEnv = async () => {
 };
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] }), UsersModule, WorkspacesModule, ChannelsModule],
   controllers: [AppController],
   providers: [
     AppService,
